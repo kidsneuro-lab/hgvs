51c85ae83bbf00db2beda35b930d2e3239749459
Peter Kneale <pkneale@cmri.org.au>
2023-11-27 16:36:48 +1100

    Use a single file

--- tests/fixtures/hgvs_normalised/normalise.py
+++ tests/fixtures/hgvs_normalised/normalise.py
@@ -5,7 +5,7 @@ import subprocess
 
 fasta = '/workspaces/hgvs/assemblies/hg38.fa'
 
-def write_file(template_path, new_file_path, line_to_append):
+def write_file_header(new_file_path, template_path):
     # Read the template file
     with open(template_path, 'r', encoding='utf-8') as template_file:
         template_content = template_file.read()
@@ -13,7 +13,9 @@ def write_file(template_path, new_file_path, line_to_append):
     # Write the template content to the new file
     with open(new_file_path, 'w', encoding='utf-8') as new_file:
         new_file.write(template_content)
-
+        
+def write_file_row(new_file_path, line_to_append):
+    
     # Append the additional line to the new file
     with open(new_file_path, 'a', encoding='utf-8') as new_file:
         new_file.write('\n' + line_to_append)
@@ -33,7 +35,10 @@ def run_bcftools_norm(input_vcf, output_vcf):
         print(f"An error occurred: {e}")
         
 def process_csv(input_file, output_file):
+    unsorted_file =  'unsorted.vcf'
+    sorted_file =  'sorted.vcf'
     
+    write_file_header(unsorted_file, 'header.vcf')
     with open(input_file, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         
@@ -45,26 +50,22 @@ def process_csv(input_file, output_file):
                 pos = row.get('Position')
                 ref = row.get('Ref')
                 alt = row.get('Alt')
-                line = f'{chr}\t{pos}\t{hgvs}\t{ref}\t{alt}\t.\t.\t.\n'
+                line = f'{chr}\t{pos}\t{hgvs}\t{ref}\t{alt}\t.\t.\t.'
                 print(f'Processing {i}')
                 
-                unsorted_file = f'/tmp/temp_unsorted_{i}.vcf'
-                sorted_file = f'/tmp/temp_sorted_{i}.vcf'
-                
-                write_file('header.vcf', unsorted_file ,line)
-                
-                run_bcftools_sort(unsorted_file, sorted_file)
-                run_bcftools_norm(sorted_file,f'{output_file}_{i}.vcf')
-                
-                os.remove(unsorted_file)
-                os.remove(sorted_file)
+                write_file_row(unsorted_file, line)
             else:
                 print(f'Skipping {hgvs}')
-            
+               
+        run_bcftools_sort(unsorted_file, sorted_file)
+        run_bcftools_norm(sorted_file, output_file)
+        os.remove(unsorted_file)
+        os.remove(sorted_file)
+                   
                 
 def main():
-    process_csv("../hgvs_downloads/output_ensemble.csv", "ensemble")
-    process_csv("../hgvs_downloads/output_variant_validator.csv", "variant_validator")
+    process_csv("../hgvs_downloads/output_ensemble.csv", "normalised_ensemble.vcf")
+    process_csv("../hgvs_downloads/output_variant_validator.csv", "normalised_variant_validator.vcf")
 
 if __name__ == '__main__':
     main()
