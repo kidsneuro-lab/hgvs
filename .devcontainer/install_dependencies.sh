d91a8ae084cdbbd1637fcfde3e8c610148044ff7
Peter Kneale <pkneale@cmri.org.au>
2023-11-28 09:29:41 +1100

    Remove unnecessary folder

--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -6,9 +6,6 @@ BCFTOOLS_VERSION="1.18"
 sudo apt-get update
 sudo apt-get install -y wget build-essential bzip2 libz-dev liblzma-dev libbz2-dev libcurl4-openssl-dev
 
-mkdir bin
-cd bin
-
 # Install bcftools
 wget -q https://github.com/samtools/bcftools/releases/download/$BCFTOOLS_VERSION/bcftools-$BCFTOOLS_VERSION.tar.bz2
 tar -vxjf bcftools-$BCFTOOLS_VERSION.tar.bz2
