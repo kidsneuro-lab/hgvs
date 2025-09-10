1fc72d2a703f10fc23811ee5761bbf39b03c8d96
h-joshi <h-joshi@users.noreply.github.com>
2025-09-10 19:15:55 +1000

    Add CLI tests and update Dockerfile for Python 3.11 support; enhance run-tests script for API testing (#44)
    * Add CLI tests and update Dockerfile for Python 3.11 support; enhance run-tests script for API testing
    
    * Add missing step to download refGene dataset in CLI tests
    
    * Add docker-compose configuration for API tests and update run-tests script
    
    * Fix argument type for prioritise_X_over_Y flag in main parser
    
    * Fix argument action for prioritise_X_over_Y flag in main parser
    
    * Fix argument action for prioritise_X_over_Y flag in main parser
    
    * Rename service from 'tests' to 'hgvs' in docker-compose configuration
    
    * Refactor run-tests script to build API tests before running them
    
    * Update run-tests script with detached mode and wait time; update expected output normalization flag in tests
    
    * Remove --abort-on-container-exit option from API tests in run-tests script
    
    * Simplify API tests execution in run-tests script by removing unnecessary options

--- Packager.Dockerfile
+++ Packager.Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10-slim
+FROM python:3.11-slim
 ARG VERSION=0.1.0
 
 # Argument for version number
--- run-tests.sh
+++ run-tests.sh
@@ -16,3 +16,29 @@ docker compose -f docker-compose-unit-tests.yml up \
   --no-log-prefix \
   --abort-on-container-exit \
   --exit-code-from tests
+
+echo "##########################################"
+echo "Building API tests"
+echo "##########################################"
+docker compose -f docker-compose-api-tests.yml build
+
+echo "##########################################"
+echo "Running API tests"
+echo "##########################################"
+docker compose -f docker-compose-api-tests.yml up \
+  --detach
+
+# Wait for 5 seconds
+sleep 5s
+
+echo "##########################################"
+echo "Calling API"
+echo "##########################################"
+curl --fail -X "POST" "http://127.0.0.1:8002/translate" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "indels_start_with_same_base": false,
+  "ignore_version": true,
+  "input": "NM_173495.3:c.1835_1839delinsGAA",
+  "normalise": true
+}'
